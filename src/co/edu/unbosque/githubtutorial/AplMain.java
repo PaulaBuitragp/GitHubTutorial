9f06d986698aef740dfa9ec62c0294eb1d4c7aa7
EstebanUribeCalderon <72117571+EstebanUribeCalderon@users.noreply.github.com>
2021-02-17 22:21:01 -0500

    Update sysout in method main

--- src/co/edu/unbosque/githubtutorial/AplMain.java
+++ src/co/edu/unbosque/githubtutorial/AplMain.java
@@ -5,7 +5,8 @@ public class AplMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-			System.out.println("Hola mundo");
+			System.out.println("Hola mundo y Hola Paula mrena divina ;)");
+			
 		}
 	}
 
