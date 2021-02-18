8ab419f544c5e29cb22b6c9001aaa4f3d2b360fc
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2021-02-17 21:48:10 -0500

    Creating new main method

---
+++ src/co/edu/unbosque/githubtutorial/AplMain.java
@@ -0,0 +1,12 @@
+package co.edu.unbosque.githubtutorial;
+
+public class AplMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			System.out.println("Hola mundo");
+		}
+	}
+
+
